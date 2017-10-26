17d58c8b8e3f55f195242fd077bee7b694e26988
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2017-10-26 10:37:24 -0700

    Add files via upload

---
+++ TransposeTab/TransposeTab/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransposeTab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string[] lines;
+        private string[] newlines;
+        private string FileName;
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files|*.txt";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                FileName = openFileDialog1.FileName;
+            else
+                return;
+            lines = System.IO.File.ReadAllLines(FileName);
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(lines);
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private string[] ScaleDown = {"C", "B", "Bb", "A", "Ab", "G", "Gb", "F", "E", "Eb", "D", "Db"};
+        private string[] ScaleUp = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+            Transpose(-1);
+        }
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+            Transpose(1);
+        }
+
+        private void Transpose(int direction)
+        {
+            int lineNum = 1;
+            foreach (var item in lines)
+            {
+                if (item.ToString().Length > 0 && lineNum > 2)
+                {
+                    Char[] letters = item.ToString().ToCharArray();
+                    if (letters[0] == '-')
+                    {
+                        for (int index = 0; index < letters.Length; index++)
+                        {
+                            if (Char.IsNumber(letters[index]))
+                            {
+                                int oldValue = Convert.ToInt16(letters[index].ToString());
+                                int newValue = 0;
+                                if (Char.IsNumber(letters[index + 1]))
+                                {
+                                    oldValue = (Convert.ToInt16(letters[index].ToString())*10) +
+                                               (Convert.ToInt16(letters[index + 1].ToString()));
+                                }
+
+                                newValue = oldValue + direction;
+                                if (newValue < 0)
+                                    Debugger.Break();
+                                if (newValue < 10 && oldValue > 9)
+                                {
+                                    letters[index] = '-';
+                                    letters[index + 1] = newValue.ToString().ToCharArray()[0];
+                                    index++;
+                                }
+                                else if (newValue < 10 && oldValue < 10)
+                                {
+                                    letters[index] = newValue.ToString().ToCharArray()[0];
+                                }
+                                else
+                                {
+                                    letters[index] = newValue.ToString().ToCharArray()[0];
+                                    letters[index + 1] = newValue.ToString().ToCharArray()[1];
+                                    index++;
+                                }
+
+                                if (index > letters.Length)
+                                    break;
+
+                            }
+                        }
+                        lines[lineNum - 1] = new String(letters);
+                    }
+                    else
+                    {
+                        for (int index = 1; index < letters.Length; index++)
+                        {
+                            if (Char.IsLetter(letters[index]) && Char.IsWhiteSpace(letters[index - 1]) &&
+                                Char.IsUpper(letters[index]) &&
+                                Array.Exists(ScaleUp, letter => letter == letters[index].ToString()))
+                            {
+                                if (index + 1 == letters.Length || letters[index + 1] == 'm' ||
+                                    Char.IsNumber(letters[index + 1]) || Char.IsWhiteSpace(letters[index + 1]))
+                                {
+                                    string oldValue = letters[index].ToString();
+                                    string newValue = String.Empty;
+                                    if(direction<0)
+                                    { 
+                                        int oldValIndex = Array.IndexOf(ScaleDown, oldValue);
+                                        if (oldValIndex < ScaleDown.Length)
+                                            newValue = ScaleDown[oldValIndex + 1];
+                                        else
+                                            newValue = ScaleDown.First();
+
+                                    }
+                                    else
+                                    {
+                                        int oldValIndex = Array.IndexOf(ScaleDown, oldValue);
+                                        if (oldValIndex < ScaleUp.Length)
+                                            newValue = ScaleUp[oldValIndex + 1];
+                                        else
+                                            newValue = ScaleUp.First();
+                                    }
+                                    if (newValue.Length > 1)
+                                    {
+                                        letters[index - 1] = newValue.ToCharArray()[0];
+                                        letters[index] = newValue.ToCharArray()[1];
+                                    }
+                                    else
+                                        letters[index] = newValue.ToCharArray()[0];
+                                }
+                            }
+                        }
+                        lines[lineNum - 1] = new String(letters);
+
+                    }
+                }
+                lineNum++;
+            }
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(lines);
+        }
+
+
+        private void Clipboard_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(ConvertStringArrayToString(lines));
+        }
+
+        static string ConvertStringArrayToString(string[] array)
+        {
+            // Concatenate all the elements into a StringBuilder.
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.AppendLine(value);
+            }
+            return builder.ToString();
+        }
+
+ 
+
+    }
+}
---
+++ TransposeTab/TransposeTab/Form1.designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TransposeTab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Close = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Up = new System.Windows.Forms.Button();
+            this.Down = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Clipboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(897, 263);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 0;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(897, 205);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 1;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(878, 270);
+            this.listBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Up
+            // 
+            this.Up.Location = new System.Drawing.Point(897, 117);
+            this.Up.Name = "Up";
+            this.Up.Size = new System.Drawing.Size(75, 23);
+            this.Up.TabIndex = 3;
+            this.Up.Text = "Up";
+            this.Up.UseVisualStyleBackColor = true;
+            this.Up.Click += new System.EventHandler(this.Up_Click);
+            // 
+            // Down
+            // 
+            this.Down.Location = new System.Drawing.Point(897, 146);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(75, 23);
+            this.Down.TabIndex = 4;
+            this.Down.Text = "Down";
+            this.Down.UseVisualStyleBackColor = true;
+            this.Down.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(897, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Transpose";
+            // 
+            // Clipboard
+            // 
+            this.Clipboard.Location = new System.Drawing.Point(897, 234);
+            this.Clipboard.Name = "Clipboard";
+            this.Clipboard.Size = new System.Drawing.Size(75, 23);
+            this.Clipboard.TabIndex = 6;
+            this.Clipboard.Text = "Clipboard";
+            this.Clipboard.UseVisualStyleBackColor = true;
+            this.Clipboard.Click += new System.EventHandler(this.Clipboard_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 295);
+            this.Controls.Add(this.Clipboard);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Down);
+            this.Controls.Add(this.Up);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Close);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Up;
+        private System.Windows.Forms.Button Down;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Clipboard;
+    }
+}
+
